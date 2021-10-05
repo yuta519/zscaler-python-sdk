8b0c04ba9d209ae1f5a6832b4adc7af0755a2605
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-10-05 11:17:09 +0900

    add new example file example_admin.py

---
+++ examples/example_admin.py
@@ -0,0 +1,43 @@
+# Please erase comment out, when you try examples below.
+
+
+# from zscaler_python_sdk.admin import create_adminuser
+# from zscaler_python_sdk.admin import fetch_adminroles
+
+# from zscaler_python_sdk.admin import fetch_adminusers
+
+
+"""
+# You can fetch adminusers
+adminusers = fetch_adminusers()
+print(adminusers)
+
+# If you manage multi ZIA account, could specify a tenant.
+adminusers = fetch_adminusers(tenant="xxxxxxxxxxxx")
+print(adminusers)
+"""
+
+
+"""
+# You can fetch adminroles
+adminroles = fetch_adminroles()
+print(adminroles)
+
+# If you manage multi ZIA account, could specify a tenant.
+adminroles = fetch_adminroles(tenant="xxxxxxxxxxxx")
+print(adminroles)
+"""
+
+
+"""
+# You can create an adminuser.
+create_adminuser_message = create_adminuser(
+    loginName="test1@zscaler.net",
+    userName="Yuta Kawamura",
+    email="test1@zscaler.net",
+    password="P@ssw0rd",
+    rolename="Super Admin",
+    tenant="xxxxxxxxxxxx",
+)
+print(create_adminuser_message)
+"""
