68510a4f1ef08b4fbd250298f12d97d4cc1879bc
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-10-11 11:16:53 +0900

    add example file for firewall

---
+++ examples/example_fw_rules.py
@@ -0,0 +1,11 @@
+# Please erase comment out, when you try examples below.
+
+# from zscaler_python_sdk.firewall_rules import fetch_firewall_rules
+
+"""
+# You can fetch users
+firewall_rules = fetch_firewall_rules(
+    tenant="xxxxxxxxxx",
+)
+print(firewall_rules)
+"""
