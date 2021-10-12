cd5c33336385a5f5144e98c18f91c08da2f567f7
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-10-12 18:20:41 +0900

    add fetchin specific firewall

--- zscaler_python_sdk/firewall_rules.py
+++ zscaler_python_sdk/firewall_rules.py
@@ -2,8 +2,14 @@ from typing import Any, Dict, Optional
 from zscaler_python_sdk.zia import api_get
 
 
-def fetch_firewall_rules(tenant: Optional[str] = None) -> Dict[str, Any]:
-    firewall_rules: Dict[str, Any] = api_get("/firewallFilteringRules", tenant)
-    # firewall_rules = sorted(firewall_rules[tenant], key=lambda x: x["order"]) for tenant in firewall_rules.keys()
-
+def fetch_firewall_rules(
+    rule_id: Optional[str] = None,
+    tenant: Optional[str] = None,
+) -> Dict[str, Any]:
+    firewall_rules: Dict[str, Any] = api_get(
+        "/firewallFilteringRules"
+        if rule_id is None
+        else f"/firewallFilteringRules/{rule_id}",
+        tenant,
+    )
     return firewall_rules
