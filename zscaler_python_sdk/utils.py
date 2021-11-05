c036895e639581d5c225bb39364bf6037a22498e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-11-05 09:53:21 +0900

    change translate return value from str to dict

--- zscaler_python_sdk/utils.py
+++ zscaler_python_sdk/utils.py
@@ -1,18 +1,19 @@
-from typing import Optional
+from typing import Dict
+from typing import List
 
 from zscaler_python_sdk import url_categories
 
 
 def translate_category_from_name_to_id(
-    category_name: str,
+    category_names: List[str],
     tenant: str,
-) -> Optional[str]:
+) -> Dict[str, str]:
     all_url_categories = url_categories.fetch_url_categories(
         isCustomOnly=True,
         tenant=tenant,
     )
-    category_id: Optional[str] = None
+    category_id_list: dict[str, str] = {}
     for cateogry in all_url_categories[tenant]:
-        if cateogry["configuredName"] == category_name:
-            category_id = cateogry["id"]
-    return category_id
+        if cateogry["configuredName"] in category_names:
+            category_id_list[cateogry["configuredName"]] = cateogry["id"]
+    return category_id_list
