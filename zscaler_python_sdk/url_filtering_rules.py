4274279bddd7ca488e152f4b5c89ad5c17e216c0
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-11-09 10:23:15 +0900

    add functions like fetching rule and update that one

--- zscaler_python_sdk/url_filtering_rules.py
+++ zscaler_python_sdk/url_filtering_rules.py
@@ -5,6 +5,7 @@ from typing import Union
 
 from zscaler_python_sdk.zia import api_get
 from zscaler_python_sdk.zia import api_post
+from zscaler_python_sdk.zia import api_put
 
 from requests.models import Response
 
@@ -40,20 +41,40 @@ def fetch_specific_url_filering_rules():
 
 def fetch_specific_url_filering_rule_id(rule_name: str, tenant: str) -> int:
     url_filtering_rules: list[str] = api_get("/urlFilteringRules", tenant)
-    target_rule_id: Optional[int] = None
-    for rule in url_filtering_rules:
-        target_rule_id = rule["id"] if rule["name"] == rule_name else None
-    return target_rule_id
+    target_rule: Optional[dict] = None
+    for rule in url_filtering_rules[tenant]:
+        if rule["name"] == rule_name:
+            target_rule = rule
+    return target_rule
 
 
-def update_url_filtering_rule(rule_name: str, tenant: str) -> str:
-    rule_id: Optional[int] = fetch_specific_url_filering_rule_id(
+def update_url_filtering_rule(
+    rule_name: str,
+    tenant: str,
+    name: Optional[str] = None,
+    order: Optional[int] = None,
+    rank: Optional[int] = None,
+    state: Optional[str] = None,
+    protocols: Optional[list] = None,
+    action: Optional[str] = None,
+) -> str:
+    target_rule: Optional[dict] = fetch_specific_url_filering_rule_id(
         rule_name=rule_name,
         tenant=tenant,
     )
-    if rule_id is None:
+    if target_rule is None:
         return "[Error] Invalied URL Filtering Rule Name"
-    # api_put("/urlFilteringRules", tenant)
+    payload: Dict[str, str] = {
+        "name": name if name is not None else target_rule["name"],
+        "order": order if order is not None else target_rule["order"],
+        "rank": rank if rank is not None else target_rule["rank"],
+        "state": state if state is not None else target_rule["state"],
+        "protocols": protocols if protocols is not None else target_rule["protocols"],
+        "action": action if action is not None else target_rule["action"],
+    }
+    print(payload)
+    message = api_put(f"/urlFilteringRules/{target_rule['id']}", payload, tenant)
+    return message
 
 
 def create_url_filering_rules(
